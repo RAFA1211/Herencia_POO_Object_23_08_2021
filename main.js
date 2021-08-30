2573be58059bbd3bff712aa295a59945647f314a
RAFA1211 <rafael.angarita.raar@gmail.com>
2021-08-30 18:45:01 -0500

    cambios 23 Agosto

---
+++ main.js
@@ -0,0 +1,93 @@
+
+
+class espada{
+    
+    constructor(arg){
+for(let[id,value]of Object.entries(arg)){
+    this[id]=value
+}
+    }
+    atacar(){
+        return this.ataque;
+    }
+}
+    class espadaHielo extends espada{
+
+    }
+
+
+    let data= {
+        ataque:30,
+        diametro: "30 cm",
+        efecto: undefined
+    }
+}
+
+const inst = new espada({ataque:30});
+console.log(inst.atacar())
+
+
+// const espada = {
+//     ataque :30,
+//     diametro : "30cm",
+//     efecto : undefined,
+//     atacar(){
+//         return this.ataque;
+//     }
+// }
+
+// const espadaHielo = Object.create(espada);
+// Object.defineProperties(espadaHielo,{
+//                         "vida":{
+//                             value: "+30",
+//                             enumerable: true
+//                         },
+//                         "efecto":{
+//                             value: "Congelar",
+//                             enumerable: true
+//                         },
+//                         "ataque":{
+//                             value: espada.ataque += 10,
+//                             enumerable: true
+//                         }
+// });
+// espadaHielo.atacar = function(){
+//     return `${this.ataque} MOD ${this.efecto} y ${this.vida} vida`;
+// }
+// console.log(espada.atacar());
+
+class espada{
+    constructor(arg){
+        for(let [id, value] of Object.entries(arg)){
+            this[id] = value;
+        }
+    }
+    atacar(){
+        return this.ataque;
+    }
+}
+class espadaHielo extends espada{
+    constructor(arg){
+        super(arg);
+        this.vida = "+30";
+        this.efecto = "Congelado";
+        this.ataque+=10;
+    }
+    atacar(){
+        return `${this.ataque} MOD ${this.efecto} y ${this.vida} vida`;
+    }
+}
+
+
+let data = {
+    ataque:30,
+    diametro: "30cm",
+    efecto: undefined
+}
+const inst = new espada(data);
+console.log(inst.atacar());
+console.log(inst);
+
+const instHielo = new espadaHielo(data);
+console.log(instHielo.atacar());
+console.log(instHielo);
